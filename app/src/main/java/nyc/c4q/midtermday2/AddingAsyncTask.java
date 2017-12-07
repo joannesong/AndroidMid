80a2a4d7c88fb1842b905802e93fd87d83b72389
Joanne Song <joannesong@ac.c4q.nyc>
2017-12-06 21:41:13 -0500

    added part01

---
+++ app/src/main/java/nyc/c4q/midtermday2/AddingAsyncTask.java
@@ -0,0 +1,49 @@
+package nyc.c4q.midtermday2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+/**
+ * Created by joannesong on 12/6/17.
+ */
+
+class AddingAsyncTask extends AsyncTask<Integer, Integer, Integer> {
+    Context context;
+    private int x = 0;
+
+    public AddingAsyncTask(Context context) {
+        this.context = context;
+    }
+
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Integer doInBackground(Integer... integers) {
+
+        for (int i = 0; i <100000 ; i++) {
+            publishProgress(i);
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(Integer integer) {
+        super.onPostExecute(integer);
+
+    }
+
+    Intent intent = new Intent(context , LoginActivity.this);
+        context.startActivity(intent);
+}
--- app/src/main/java/nyc/c4q/midtermday2/LoopActivity.java
+++ app/src/main/java/nyc/c4q/midtermday2/LoopActivity.java
@@ -1,13 +1,64 @@
 package nyc.c4q.midtermday2;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoopActivity extends AppCompatActivity {
+    TextView textView = (TextView) findViewById(R.id.text);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loop);
+
+
     }
+
+    class AddingAsyncTask extends AsyncTask<Integer, Integer, Integer> {
+        Context context;
+        private int x = 0;
+
+        public AddingAsyncTask(Context context) {
+            this.context = context;
+        }
+
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... integers) {
+
+            for (int i = 0; i <100000 ; i++) {
+                publishProgress(i);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            textView.setText("Loops completed: " + values);
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+
+            textView.setText("Loops completed: " + integer);
+        }
+
+        Intent intent = new Intent(context , LoginActivity.this);
+        context.startActivity(intent);
+    }
+
 }
