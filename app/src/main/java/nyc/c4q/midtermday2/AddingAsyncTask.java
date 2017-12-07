233ba6bbcb6745777eab74e37b0934878414c824
Joanne Song <joannesong@ac.c4q.nyc>
2017-12-06 20:05:53 -0500

    added par02

---
+++ app/src/main/java/nyc/c4q/midtermday2/AddingAsyncTask.java
@@ -0,0 +1,41 @@
+package nyc.c4q.midtermday2;
+
+import android.os.AsyncTask;
+import android.support.v7.view.menu.MenuView;
+import android.widget.TextView;
+
+/**
+ * Created by joannesong on 12/6/17.
+ */
+
+public class AddingAsyncTask  {
+//    extends AsyncTask<Integer, Integer, Integer>
+
+//    private int x = 0;
+//
+//
+//    @Override
+//    protected void onPreExecute() {
+//        super.onPreExecute();
+//    }
+//
+//    @Override
+//    protected Integer doInBackground(Integer... integers) {
+//        integers
+//        for(Integer i = progress;i < 100000; i++  ){
+//
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    protected void onProgressUpdate(Integer... values) {
+//        super.onProgressUpdate(values);
+//        System.out.println("Loops completed: " + values);
+//    }
+//
+//    @Override
+//    protected void onPostExecute(Integer integer) {
+//        super.onPostExecute(integer);
+//    }
+}
--- app/src/main/java/nyc/c4q/midtermday2/ListActivity.java
+++ app/src/main/java/nyc/c4q/midtermday2/ListActivity.java
@@ -2,16 +2,12 @@ package nyc.c4q.midtermday2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class LoopActivity extends AppCompatActivity {
-    public TextView textView;
+public class ListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loop);
-
-        textView = (TextView) findViewById(R.id.text);
+        setContentView(R.layout.activity_list);
     }
 }
